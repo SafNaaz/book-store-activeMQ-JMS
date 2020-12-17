b0a9bad8920bb8c4d6fbcaf9448c8e85b9f09a3b
Safnas <iamsafnas@gmail.com>
2020-12-17 17:28:48 +0530

    custom listener- calling processingservice once message is received

--- src/main/java/com/mdrsolutions/SpringJmsExample/config/JmsConfig.java
+++ src/main/java/com/mdrsolutions/SpringJmsExample/config/JmsConfig.java
@@ -12,7 +12,6 @@ import org.springframework.jms.annotation.JmsListenerConfigurer;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerEndpointRegistrar;
 import org.springframework.jms.config.SimpleJmsListenerEndpoint;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MarshallingMessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
@@ -23,7 +22,7 @@ import org.springframework.oxm.xstream.XStreamMarshaller;
 @Configuration
 public class JmsConfig implements JmsListenerConfigurer {
 
-//    @Bean
+    @Bean
     public MessageConverter jacksonJmsMessageConverter(){
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
@@ -31,14 +30,14 @@ public class JmsConfig implements JmsListenerConfigurer {
         return converter;
     }
 
-    @Bean
+//    @Bean
     public MessageConverter xmlMarshallingMessageConverter(){
         MarshallingMessageConverter converter = new MarshallingMessageConverter(xmlMarshaller());
         converter.setTargetType(MessageType.TEXT);
         return converter;
     }
 
-    @Bean
+//    @Bean
     public XStreamMarshaller xmlMarshaller(){
         XStreamMarshaller marshaller = new XStreamMarshaller();
         marshaller.setSupportedClasses(Book.class, Customer.class, BookOrder.class);
@@ -55,8 +54,8 @@ public class JmsConfig implements JmsListenerConfigurer {
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(){
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory());
-//        factory.setMessageConverter(jacksonJmsMessageConverter());
-        factory.setMessageConverter(xmlMarshallingMessageConverter());
+        factory.setMessageConverter(jacksonJmsMessageConverter());
+//        factory.setMessageConverter(xmlMarshallingMessageConverter());
         return factory;
     }
 
--- src/main/java/com/mdrsolutions/SpringJmsExample/service/jms/WareHouseReceiverService.java
+++ src/main/java/com/mdrsolutions/SpringJmsExample/service/jms/WareHouseReceiverService.java
@@ -3,6 +3,7 @@ package com.mdrsolutions.SpringJmsExample.service.jms;
 import com.mdrsolutions.SpringJmsExample.pojos.BookOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +12,13 @@ public class WareHouseReceiverService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WareHouseReceiverService.class);
 
+    @Autowired
+    WarehouseProcessingService warehouseProcessingService;
+
     @JmsListener(destination = "book.order.queue")
     public void receive(BookOrder bookOrder){
         LOGGER.info("received a message");
         LOGGER.info("Message is == "+bookOrder);
+        warehouseProcessingService.processOrder(bookOrder);
     }
 }
