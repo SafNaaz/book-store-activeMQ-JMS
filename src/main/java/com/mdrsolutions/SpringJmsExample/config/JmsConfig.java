9a5a2e7fbad4b69ac9a4a5da985b16c716145f60
Safnas <iamsafnas@gmail.com>
2020-12-17 14:33:43 +0530

    switching to xml message converter

--- src/main/java/com/mdrsolutions/SpringJmsExample/config/JmsConfig.java
+++ src/main/java/com/mdrsolutions/SpringJmsExample/config/JmsConfig.java
@@ -1,5 +1,8 @@
 package com.mdrsolutions.SpringJmsExample.config;
 
+import com.mdrsolutions.SpringJmsExample.pojos.Book;
+import com.mdrsolutions.SpringJmsExample.pojos.BookOrder;
+import com.mdrsolutions.SpringJmsExample.pojos.Customer;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,19 +10,35 @@ import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MarshallingMessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import org.springframework.oxm.xstream.XStreamMarshaller;
 
 @EnableJms
 @Configuration
 public class JmsConfig {
 
-    @Bean
+//    @Bean
     public MessageConverter jacksonJmsMessageConverter(){
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         return converter;
+    }
+
+    @Bean
+    public MessageConverter xmlMarshallingMessageConverter(){
+        MarshallingMessageConverter converter = new MarshallingMessageConverter(xmlMarshaller());
+        converter.setTargetType(MessageType.TEXT);
+        return converter;
+    }
+
+    @Bean
+    public XStreamMarshaller xmlMarshaller(){
+        XStreamMarshaller marshaller = new XStreamMarshaller();
+        marshaller.setSupportedClasses(Book.class, Customer.class, BookOrder.class);
+        return marshaller;
     }
 
     @Bean
@@ -32,7 +51,8 @@ public class JmsConfig {
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(){
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory());
-        factory.setMessageConverter(jacksonJmsMessageConverter());
+//        factory.setMessageConverter(jacksonJmsMessageConverter());
+        factory.setMessageConverter(xmlMarshallingMessageConverter());
         return factory;
     }
 }
